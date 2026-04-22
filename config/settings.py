9d4db1004d221d71ace43d1eabcfa25961f3dbe6
Safwan MV <safwanmv007@gmail.com>
2026-04-22 14:06:57 +0530

    fix: add TEMPLATES config

--- config/settings.py
+++ config/settings.py
@@ -127,3 +127,20 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # ========================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
