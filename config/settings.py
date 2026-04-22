37a113d639bd45ebfc64d9a59d8fa383a35bb54a
Safwan MV <safwanmv007@gmail.com>
2026-04-22 14:26:03 +0530

    fix: switch to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -61,13 +61,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # DATABASE
 # ========================
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
+}
 # ========================
 # AUTH
 # ========================
